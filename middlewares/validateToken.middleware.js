27a97be88b89d0950f807b68678145c3cb0f06ea
Joaquiniturriaga <joa.iturriaga@duocuc.cl>
2026-05-17 15:59:11 -0400

    feat:middlewares

---
+++ middlewares/validateToken.middleware.js
@@ -0,0 +1,17 @@
+const validateToken = (req, res, next) => {
+    const userId = req.headers['x-user-id'];
+    const userRole = req.headers['x-user-role'];
+
+    if (!userId) {
+        return res.status(401).json({ error: 'Token required' });
+    }
+
+    req.user = {
+        id: parseInt(userId),
+        role: userRole,
+    };
+
+    next();
+};
+
+module.exports = validateToken;
